4defc4808a0bbddf08039252f90824f1213ee3fa
sgrmhdk51 <sgrmhdk51@gmail.com>
2020-10-30 11:52:05 +0530

    Added Application state to manage the API calls efficiently

--- client/src/components/Singularity/OwnerView/CafeManagement/DIgitizedMenu/DisplayMenu/Components/DmenuDisplay.js
+++ client/src/components/Singularity/OwnerView/CafeManagement/DIgitizedMenu/DisplayMenu/Components/DmenuDisplay.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import LogoPiattoTagLine from 'components/Singularity/ApplicationView/MoleculeComponent/LogoWithPiattoTagline';
 import { CenterAlignedColumnContainer } from 'styles/Singularity/Style1.0/ContainerStyles';
 import { DMenuCategoryContainer } from 'styles/Singularity/OwnerView/CafeManagement/DigitizedMenu/index.js';
+import { ThemeAnimationContainer } from 'styles/Singularity/Style1.0/Animations';
 import CategoryCard from 'components/Singularity/OwnerView/CafeManagement/DIgitizedMenu/DisplayMenu/Components/ComponentDesign/CategoryCard.js';
 import applicationContext from 'Context/ApplicationContext/applicationContext.js';
 
@@ -11,20 +12,30 @@ const DmenuDisplay = () => {
 
   return (
     <>
-      <CenterAlignedColumnContainer>
-        <LogoPiattoTagLine backGroundcolor="rgba(0,0,0,0.8)" />
-        <DMenuCategoryContainer backGroundcolor="rgba(0,0,0,0.8)">
-          {categoryData.map((category, categoryIndex) => {
-            return (
-              <CategoryCard
-                value={category.category}
-                description={category.category}
-                identifier={category.categoryIdentifier}
-              />
-            );
-          })}
-        </DMenuCategoryContainer>
-      </CenterAlignedColumnContainer>
+      <ThemeAnimationContainer
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          ease: 'easeOut',
+          duration: 0.8
+        }}
+        exit={{ opacity: 0 }}
+      >
+        <CenterAlignedColumnContainer>
+          <LogoPiattoTagLine backGroundcolor="rgba(0,0,0,0.8)" />
+          <DMenuCategoryContainer backGroundcolor="rgba(0,0,0,0.8)">
+            {categoryData.map((category, categoryIndex) => {
+              return (
+                <CategoryCard
+                  value={category.category}
+                  description={category.category}
+                  identifier={category.categoryIdentifier}
+                />
+              );
+            })}
+          </DMenuCategoryContainer>
+        </CenterAlignedColumnContainer>
+      </ThemeAnimationContainer>
     </>
   );
 };
